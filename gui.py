f1fb2c5532e5ab8c216b7ab390fc10e6a0a600f9
Joshua Caponigro <joshcap@email.unc.edu>
2022-05-18 14:07:54 -0400

    1.1

---
+++ gui.py
@@ -0,0 +1,142 @@
+from PyQt6.QtWidgets import *
+import rokutest as roku
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+import sys
+
+
+
+class Remote(QDialog):
+    def __init__(self, parent=None):
+        super(Remote, self).__init__(parent)
+        self.initUI()
+        self.show()
+
+
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        # Channels/Inputs
+        xbox = QPushButton('Xbox')
+        netflix = QPushButton('Netflix')
+        hulu = QPushButton('Hulu')
+        disneyplus = QPushButton('Disney Plus')
+        hbomax = QPushButton('HBO Max')
+
+        # Shows for quick access
+        southpark = QPushButton('Southpark')
+        modernfamily = QPushButton('Modern Family')
+        got = QPushButton('GoT')
+        workaholics = QPushButton('Workaholics')
+
+        # Remote features
+        home = QPushButton('Home')
+        select = QPushButton('Select')
+        up = QPushButton('Up')
+        down = QPushButton('Down')
+        right = QPushButton('Right')
+        left = QPushButton('Left')
+        volup = QPushButton('Volume Up')
+        voldown = QPushButton('Volume Down')
+        poweroff = QPushButton('Power Off')
+
+        # Other
+        music = QPushButton('Music')
+        start = QCheckBox('Start')
+
+        # Volume slider
+        sld = QSlider(Qt.Orientation.Horizontal, self)
+        sld.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        sld.setGeometry(10, 40, 140, 30)
+        sld.valueChanged[int].connect(self.changeValue)
+        sld.setValue(25)
+        sld.setSingleStep(5)
+        sld.setTickInterval(5)
+        sld.setTickPosition(QSlider.TickPosition.TicksBelow)
+        sld.setMinimum(0)
+        sld.setMaximum(50)
+
+        # Add each button to layout
+        layout.addWidget(start)
+        layout.addWidget(poweroff)
+        layout.addWidget(home)
+        layout.addWidget(select)
+        layout.addWidget(up)
+        layout.addWidget(down)
+        layout.addWidget(right)
+        layout.addWidget(left)
+        layout.addWidget(volup)
+        layout.addWidget(voldown)
+        layout.addWidget(xbox)
+        layout.addWidget(netflix)
+        layout.addWidget(hulu)
+        layout.addWidget(disneyplus)
+        layout.addWidget(hbomax)
+        layout.addWidget(southpark)
+        layout.addWidget(modernfamily)
+        layout.addWidget(got)
+        layout.addWidget(workaholics)
+        layout.addWidget(music)
+        layout.addWidget(sld)
+        
+    
+
+        # Link buttons to functions in rokutest.py
+        xbox.clicked.connect(roku.play_xbox)
+        netflix.clicked.connect(roku.watch_netflix)
+        hulu.clicked.connect(roku.watch_hulu)
+        hbomax.clicked.connect(roku.watch_hbo_max)
+        disneyplus.clicked.connect(roku.watch_disney_plus)
+        southpark.clicked.connect(roku.southpark)
+        got.clicked.connect(roku.game_of_thrones)
+        modernfamily.clicked.connect(roku.modernfamily)
+        workaholics.clicked.connect(roku.workaholics)
+        home.clicked.connect(roku.home)
+        select.clicked.connect(roku.select)
+        up.clicked.connect(roku.up)
+        down.clicked.connect(roku.down)
+        right.clicked.connect(roku.right)
+        left.clicked.connect(roku.left)
+        volup.clicked.connect(roku.volume_up)
+        voldown.clicked.connect(roku.volume_down)
+        poweroff.clicked.connect(roku.power)
+        music.clicked.connect(roku.music)
+        start.stateChanged.connect(lambda:self.btnstate(start))
+
+        # Window, Layout Options
+        self.setWindowTitle("Remote")
+        self.setLayout(layout)
+
+
+    def changeValue(self, value):
+        initial_volume: int = 25
+        volume_level = self.sender().value()
+        if volume_level > initial_volume:
+            for i in range(volume_level-initial_volume):
+                roku.volume_up()
+            initial_volume = volume_level
+        if volume_level < initial_volume:
+            for i in range(initial_volume-volume_level):
+                roku.volume_down()
+            initial_volume = volume_level
+
+
+    def btnstate(self,start):
+      if start.text() == "Start":
+         if start.isChecked() == True:
+            roku.set_start()
+            roku.volume_up()
+            print("Start selected")
+         else:
+            roku.set_start_off()
+            print("Start deselected")
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    remote = Remote()
+    remote.show()
+    sys.exit(app.exec()) 
